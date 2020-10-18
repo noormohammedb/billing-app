4b98778edd8f7dd6e9497f2bc93b87a73c8c9ef7
Noor Mohammed B <nooru.mohad@gmail.com>
2020-10-18 18:40:08 +0530

    admin login demo db connection promised db operations in seperate file password validation in admin router

---
+++ db_files/db_operations.js
@@ -0,0 +1,20 @@
+const { connect_db, client } = require('./db_connection')
+
+function adminLogin(sQuery) {
+   return new Promise(async (resolve, reject) => {
+      try {
+         let db = await connect_db()
+         let data = await db.collection('admin').find({ email: sQuery.email }).toArray();
+         // console.log(data);
+         resolve(data)
+      } catch (e) {
+         console.error(e);
+         console.log('error in adminLogin');
+         reject(e)
+      } finally {
+         // await client.close();
+      }
+   })
+}
+
+module.exports = { adminLogin }
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { adminLogin } = require('../db_files/db_operations')
+
 router.get('/', (req, res) => {
    let hbsObject = {
       title: "login-admin"
@@ -10,13 +12,31 @@ router.get('/', (req, res) => {
 
 router.get('/admin-login', (req, res) => {
    // if not logedin redirected to /
-   res.end('login get');
+   // res.end('login get');
+   res.redirect('/admin')
 });
 
-router.post('/admin-login', (req, res) => {
-   console.log('getting post req');
+router.post('/admin-login', async (req, res) => {
    console.dir(req.body);
-   res.end('working  ' + req.body.email);
+   let searchData = {
+      email: req.body.email,
+      password: req.body.password
+   };
+   let db_data = await adminLogin(searchData);
+   console.log(db_data);
+   // console.log(db_data.length);
+   if (!db_data.length) {
+      console.log('no data', db_data.length);
+      res.status(400).send('no user found')
+   } else if (db_data[0].password === searchData.password) {
+      console.log('password matched');
+      res.status(200).send('loged in').end();
+   } else {
+      console.log('password Missmatch');
+      res.status(400).send('Password Missmatch').end();
+   }
+
+   res.end(`working  ${searchData.email}     ${searchData.password} `);
 });
 
 module.exports = router;
